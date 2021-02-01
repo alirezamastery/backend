e39fc060b4d400aa362640efdc3e4b0db99ce90c
Alireza Mastery <alireza.mastery@gmail.com>
2021-02-01 22:58:49 +0330

    extracting category hierarchy from user input with pk

--- backend/admin.py
+++ backend/admin.py
@@ -52,52 +52,60 @@ class MyAdminSite(admin.AdminSite):
         context['from_me'] = 'this is it'
         context['selectable_categories'] = list()
         context['selectable_categories'].append({'selected': None,
+                                                 'selected_pk': None,
                                                  'options':  Category.objects.filter(level=0)
                                                  })
         context['leaves'] = None
-
-        print(context['selectable_categories'])
-
+        # note that each time a request is sent, we are building the context from zero
         if request.method == 'POST':
             counter = 0
+            print('start')
             while True:  # start going down the category hierarchy
+                print('-'*50)
                 selected_category = None
                 try:
                     selected_category = request.POST.get(f'category_selector_{counter}')
+                    print(f'selected_category: {selected_category}')
                 except:
                     break
                 if selected_category:
                     # if the user has selected a category for this level in the category hierarchy,
                     # get the children of the selected category which will become the options for the next level
-                    context['selectable_categories'][counter]['selected'] = selected_category
+                    selected_category_obj = Category.objects.get(pk=selected_category)
+                    context['selectable_categories'][counter]['selected'] = selected_category_obj.name
+                    context['selectable_categories'][counter]['selected_pk'] = selected_category_obj.pk
                     # in this section we check if the data in request.POST for this level, is a child of
                     # the previous category.so if the user changes a selection, all the next 'select' tags
                     # in the front end will be removed
                     if counter > 0:
-                        parent = context['selectable_categories'][counter - 1]['selected']
-                        parent_obj = Category.objects.get(name=parent)
-                        parent_options_names = [option.name for option in parent_obj.get_children()]
-                        if selected_category not in parent_options_names:
+                        parent_pk = context['selectable_categories'][counter - 1]['selected_pk']
+                        parent_obj = Category.objects.get(pk=parent_pk)
+                        parent_options_pk = [str(option.pk) for option in parent_obj.get_children()]
+                        print(selected_category, parent_options_pk)
+                        if selected_category not in parent_options_pk:
+                            print('done')
                             break
-                    selected_category_obj = Category.objects.get(name=selected_category)
                     children = selected_category_obj.get_children()
                     # if the selected category has no children it means we have reached a leaf node and
                     # should start looking for the products related to this node
                     if not children:
+                        # TODO find a way to get product obj.now we only have a name which is not enough
                         selected_product = None
                         try:
-                            selected_product = request.POST.get(f'leaves_selector')
+                            selected_product = request.POST.get(f'product_selector')
                         except:
                             pass
                         if selected_product:
                             context['leaves'] = {'selected': selected_product,
                                                  'options':  selected_category_obj.products.all()
                                                  }
+                            # --- do your thing here ---
                             break
                         context['leaves'] = {'selected': None,
                                              'options':  selected_category_obj.products.all()
                                              }
                         break
+                    # if the selected category has children, continue going down the hierarchy
                     context['selectable_categories'].append({'selected': None,
                                                              'options':  children
                                                              })
